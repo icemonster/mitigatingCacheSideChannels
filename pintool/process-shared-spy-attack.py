751783f73f9e9b7f1ba24425735cb47548acc8dd
ice <nunossabino@hotmail.com>
2021-12-06 01:21:04 -0500

    Beautify shared core experiments results

--- pintool/process-shared-spy-attack.py
+++ pintool/process-shared-spy-attack.py
@@ -18,6 +18,8 @@ for noise in range(1,100,2):
     print(noise, '-->', results[-1])
 
 plt.plot(list(range(1, 100, 2)), results)
-plt.axis([0, 100, 0, 1024])
+plt.axis([0, 100, 400, 1024])
 plt.axline((0,512), (100, 512)) # Expected correct number of bits for random oracle
+plt.xlabel('Noise')
+plt.ylabel('Correct bits')
 plt.show()
